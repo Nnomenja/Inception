c65629d05e904f937d54ecf1373d4d2a652e1ea2
Nnomenja <mamenosoa42@gmail.com>
2026-05-16 16:59:43 +0300

    feat: add FTP server configuration and setup scripts; include Dockerfile and vsftpd configuration

---
+++ srcs/requirements/ftp-server/tools/setup-ftp-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+set -e
+
+CONFIG_PATH="/etc/vsftpd/vsftpd.conf"
+FTP_DIR="/srv/ftpfiles"
+
+# Default values
+: "${FTP_USER:?Need FTP_USER}"
+: "${FTP_PASSWORD:?Need FTP_PASSWORD}"
+
+if [ -f "$FTP_PASSWORD" ]; then
+    FTP_PASSWORD=$(cat "$FTP_PASSWORD" | tr -d '\n\r')
+fi
+
+echo "pass: $FTP_PASSWORD $FTP_USER "
+
+if ! id "$FTP_USER" >/dev/null 2>&1; then
+    adduser -D -h "$FTP_DIR" "$FTP_USER"
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+fi
+
+# Create directories
+mkdir -p "$FTP_DIR"
+chown -R "$FTP_USER:$FTP_USER" "$FTP_DIR"
+
+exec "$@" "$CONFIG_PATH"
